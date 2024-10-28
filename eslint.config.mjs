1887a6978a6fc3186b1e47fcb96c95bb3f955faa
Philip Neuffer <neph1015@h-ka.de>
2024-10-28 09:21:22 +0100

    Update eslint.config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,13 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import eslintConfigESLint from "eslint-config-eslint";
+import eslintConfigESLintFormatting from "eslint-config-eslint/formatting";
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.node }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+    { files: ["**/*.{js,mjs,cjs,ts}"] },
+    { languageOptions: { globals: globals.node } },
+    pluginJs.configs.recommended,
+    ...eslintConfigESLint,
+    eslintConfigESLintFormatting
 ];
