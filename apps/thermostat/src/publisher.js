5c5db3756a1bbaf9fd61aa9ffe9751bcc52538a8
Philip Neuffer <neph1015@h-ka.de>
2024-11-01 09:23:47 +0100

    Add ESLint configuration and improve publisher.js documentation

--- apps/thermostat/src/publisher.js
+++ apps/thermostat/src/publisher.js
@@ -1,9 +1,16 @@
 import { client } from "./main.js";
 
+/**
+ * Publishes a random temperature to a given topic at specified intervals.
+ * @param {string} topic The topic to publish the temperature to.
+ * @param {number} publishIntervall The interval in milliseconds at which to publish the temperature.
+ * @returns {void}
+ */
 function publishTemperature(topic, publishIntervall) {
     setInterval(() => {
         do {
-            // Absichtlich var statt let, da block scope hier unpraktisch ist
+
+            // eslint-disable-next-line no-var -- var is used here because block scope is annoying in this case
             var temperature = Math.floor(Math.random() * 100);
         } while (temperature > 30);
 
