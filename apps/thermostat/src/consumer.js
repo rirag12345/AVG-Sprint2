11ebd7b6620c3114a21dbde9a376611cbc689d01
Philip Neuffer <neph1015@h-ka.de>
2024-11-02 08:09:24 +0100

    Rename variables for enhanced clarity

--- apps/thermostat/src/consumer.js
+++ apps/thermostat/src/consumer.js
@@ -1,6 +1,6 @@
 import { CLIENT, ROOM } from "./main.js";
 
-const placeholder = "-control";
+const suffix = "-control";
 
 /**
  * Subscribes to a given topic and prints the received messages to the console.
@@ -10,14 +10,14 @@ const placeholder = "-control";
  */
 function pollTemperature(topic) {
     CLIENT.subscribe(topic);
-    CLIENT.subscribe(`topic${placeholder}`);
+    CLIENT.subscribe(`topic${suffix}`);
     CLIENT.on("message", (receivedTopic, receivedMessage) => {
         if (receivedTopic === topic) {
             // eslint-disable-next-line no-console -- this is a CLI application
             console.log(
                 `The temperature in the ${ROOM} is ${receivedMessage.toString()}°C.`
             );
-        } else if (receivedTopic === `topic${placeholder}`) {
+        } else if (receivedTopic === `topic${suffix}`) {
             // eslint-disable-next-line no-console -- this is a CLI application
             console.log(
                 `Received message on topic ${receivedTopic}: ${receivedMessage.toString()}`
