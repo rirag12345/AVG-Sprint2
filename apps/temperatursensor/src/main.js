29a6fd37c12017651927d364a87b05f7e322bc6b
Philip Neuffer <neph1015@h-ka.de>
2024-11-02 08:36:15 +0100

    Refactor some variables

--- apps/temperatursensor/src/main.js
+++ apps/temperatursensor/src/main.js
@@ -13,25 +13,25 @@ import mqtt from "mqtt";
  * The connection string to connect to the message broker.
  * @constant {string}
  */
-const CONNECTION_STRING = "mqtt://localhost:1883";
+const connectionString = "mqtt://localhost:1883";
 
 /**
  * The mqtt client for the message broker.
  * @constant {mqtt.Client}
  */
-const CLIENT = mqtt.connect(CONNECTION_STRING);
+const CLIENT = mqtt.connect(connectionString);
 
 /**
  * The room in which the temperatursensor is located.
  * @constant {string}
  */
-const ROOM = process.argv[2];
+const room = process.argv[2];
 
 /**
  * The topic to publish and subscribe to.
  * The first argument passed to node is appended and inidcates the room in which the temperaturesensor is located.
  */
-const topic = `temperatursensor-${ROOM}`;
+const topic = `temperatursensor-${room}`;
 
 /**
  * The intervall after which the temperature is published in milliseconds.
@@ -41,8 +41,6 @@ const publishIntervall = 2000;
 
 CLIENT.on("connect", () => {
     publishTemperature(topic, publishIntervall);
-    console.log("is this run?");
-
 });
 
 export { CLIENT };
