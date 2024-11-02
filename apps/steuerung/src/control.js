996fea70bce9639731b8a3779f804f13132f9094
Felix <152805046+felix4132@users.noreply.github.com>
2024-11-02 15:50:09 +0100

    Update: control

--- apps/steuerung/src/control.js
+++ apps/steuerung/src/control.js
@@ -10,13 +10,38 @@
 import { CLIENT } from "./main.js";
 
 /**
+ * The topics the control is managing.
+ */
+const topics = [];
+
+/**
  * Starts the control.
- * @author Felix Jaeger
  * @returns {void}
  */
 export function start() {
-    // CLIENT.subscribe();
-    CLIENT.on("message", (receivedTopic, receivedMessage) => {
-        // TODO: Implement control logic.
+    console.log("Control started.");
+    topicHandler();
+    messageHandler();
+}
+
+/**
+ * Handles the topics.
+ */
+async function topicHandler() {
+    await CLIENT.on("packetsend", (packet) => {
+        console.log(`Received packet on topic ${receivedTopic = packet.topic}`);
+        if(!topics.includes(receivedTopic)) {
+            topics.Add(receivedTopic);
+            CLIENT.subribe(receivedTopic);
+        }
+    });
+}
+
+/**
+ * Handles the messages.
+ */
+async function messageHandler() {
+    await CLIENT.on("message", (receivedTopic, receivedMessage) => {
+        console.log(`Received message on topic ${receivedTopic}: ${receivedMessage.toString()}`);
     });
 }
