ab0742bb5939af68de7278b780cd76baa56f5d1f
Philip Neuffer <neph1015@h-ka.de>
2024-11-02 08:06:33 +0100

    Add logic for temperatusensor

---
+++ apps/temperatursensor/src/publisher.js
@@ -0,0 +1,17 @@
+/**
+ * Publishes a (pseudo)random temperature to a given topic at specified intervals.
+ * @author Philip Neuffer
+ * @param {string} topic The topic to publish the temperature to.
+ * @param {number} publishIntervall The interval in milliseconds after which to publish the temperature.
+ * @returns {void}
+ */
+function publishTemperature(topic, publishIntervall) {
+    setInterval(() => {
+        do {
+            // eslint-disable-next-line no-var -- this is a CLI application
+            var temperature = Math.floor(Math.random() * 100);
+        } while (temperature > 30);
+
+        CLIENT.publish(topic, temperature.toString());
+    }, publishIntervall);
+}
